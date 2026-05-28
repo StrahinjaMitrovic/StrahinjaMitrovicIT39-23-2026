f5d7b472573f467a0eaf44dc137f1ff8aca10e10
StrahinjaMitrovic <167026464+StrahinjaMitrovic@users.noreply.github.com>
2026-05-28 12:20:58 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+console.log("Music Shop Website Loaded");
+
+document.querySelector("form")
+.addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+    alert("Poruka uspešno poslata!");
+
+});
